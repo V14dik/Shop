0e404d8781c8c8b6f1f484991aa8ab1a05bc7cd3
V14dik <cherenkevich.vlad@gmail.com>
2022-11-06 20:34:16 +0300

    Add scss to project and some styles for product

--- src/components/ProductList/Product/Product.js
+++ src/components/ProductList/Product/Product.js
@@ -1,4 +1,21 @@
+import "./Product.scss";
+
 export function Product(props) {
-  console.log(props.product);
-  return <div className="product">Product</div>;
+  return (
+    <div className="product">
+      <img
+        className="product-img"
+        src={props.product.image}
+        alt="Product image"
+      />
+      <div className="about-product">
+        <div className="product-name">{props.product.name}</div>
+        <span>{props.product.description}</span>
+        <div className="buy-container">
+          <div className="product-price">{props.product.price}$</div>
+          <button className="buy-btn">Buy</button>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,12 +1,18 @@
 import { Product } from "./Product/Product";
 import { store } from "../..";
+import "./ProductList.scss";
 
 export function ProductList() {
   const products = store.getState().product.products;
   return (
-    <div>
-      {Object.keys(products).map((product) => {
-        <Product product={product} />;
+    <div className="products">
+      {Object.keys(products).map((index) => {
+        return (
+          <Product
+            key={products[index].name + index}
+            product={products[index]}
+          />
+        );
       })}
     </div>
   );
